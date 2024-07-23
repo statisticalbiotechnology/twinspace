0b3a61f2d2b92b33e57b67f7e1aea87a76904967
ZAHRA ELHAMRAOUI <50144165+zahrael97@users.noreply.github.com>
2024-07-23 13:34:50 +0200

    Update Prosit_Koina_prediction.py

--- MSCI/Preprocessing/Prosit_Koina_prediction.py
+++ MSCI/Preprocessing/Prosit_Koina_prediction.py
@@ -2,7 +2,6 @@ import requests
 import pandas as pd
 import re
 
-# Dummy data for AA_MASSES, MOD_MASSES, MASSES to avoid errors
 AA_MASSES = {'A': 71.03711, 'R': 156.10111, 'N': 114.04293, 'D': 115.02694, 'C': 103.00919,
              'E': 129.04259, 'Q': 128.05858, 'G': 57.02146, 'H': 137.05891, 'I': 113.08406,
              'L': 113.08406, 'K': 128.09496, 'M': 131.04049, 'F': 147.06841, 'P': 97.05276,
