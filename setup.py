7289afdbecdab26f3f933c00dea77b5d4f3756c8
ZAHRA ELHAMRAOUI <50144165+zahrael97@users.noreply.github.com>
2024-08-21 14:51:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
-# Define your package's dependencies
-requirements = ['Click>=7.0', ]
-
-# Define your test dependencies
-test_requirements = ["matchms", " scipy"]
+requirements = [
+    'Click>=7.0',
+    'streamlit',
+    'matchms',
+    'scipy'
+]
 
 setup(
     author="Zahra ELHAMRAOUI",
@@ -19,17 +18,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="MSCI assesses peptide fragmentation spectra information content.",
-    entry_points={
-        'console_scripts': [
-            # Define any command-line scripts here, if applicable
-        ],
-    },
     install_requires=requirements,
     license="MIT license",
     include_package_data=True,
@@ -37,7 +28,7 @@ setup(
     name='MSCI',
     packages=find_packages(include=['MSCI', 'MSCI.*']),
     test_suite='tests',
-    tests_require=test_requirements,
+    tests_require=requirements,
     url='https://github.com/proteomicsunitcrg/MSCI',
     version='0.1.0',
     zip_safe=False,
