6e7a84ed2443e8c884fdafc3578e324754ee75b1
Zahra Elhamraoui <50144165+zahrael97@users.noreply.github.com>
2025-02-12 20:04:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     test_suite='tests',
     tests_require=requirements,
     url='https://github.com/proteomicsunitcrg/MSCI',
-    version='0.1.0',
+    version='0.2.0',
     zip_safe=False,
 )
